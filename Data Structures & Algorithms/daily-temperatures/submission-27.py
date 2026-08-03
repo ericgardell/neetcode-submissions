480371b3ab293d475ee39011e6d053e17e3ecd37
ericgardell <38889337+ericgardell@users.noreply.github.com>
2026-08-03 11:05:44 -0400

    Add: daily-temperatures - submission-27

---
+++ Data Structures & Algorithms/daily-temperatures/submission-27.py
@@ -0,0 +1,22 @@
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        result = [0] * len(temperatures)
+
+        # Indices of possible future warmer days
+        stack = []
+
+        for i in range(len(temperatures) - 1, -1, -1):
+            current_temp = temperatures[i]
+            print(f"day {i}, {current_temp}, stack temp {temperatures[stack[-1]] if stack else None}, {stack}, {result}")
+            # These days cannot answer the current day because
+            # they are colder than or equal to the current temperature.
+            while stack and temperatures[stack[-1]] <= current_temp:
+                stack.pop()
+
+            # The top is now the nearest warmer future day.
+            if stack:
+                result[i] = stack[-1] - i
+
+            stack.append(i)
+
+        return result
